c632021052f384a78e796422bfac46af8b8899fe
MARK_MERSENSKI <mjmersenski@quad.com>
2019-12-22 04:23:13 -0600

    Added main message component to app.

---
+++ src/app/mainMessage/mainMessage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'pm-mainMessage',
+    templateUrl: './mainMessage.component.html'
+})
+
+export class MainMessageComponent {
+    title: string = "MerskiDev";
+}
